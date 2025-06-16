06e1c1043f8d99060b650bf0463fa23ac87228b5
Prakash kumar <prakashkr2894@gmail.com>
2025-06-16 14:58:53 +0530

    spring initialize

---
+++ src/main/java/com/example/PrakashPortfolioApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrakashPortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrakashPortfolioApplication.class, args);
+	}
+
+}
