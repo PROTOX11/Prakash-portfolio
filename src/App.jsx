017c88b82bff69b0e14e800f08d453217140c790
Prakash kumar <prakashkr2894@gmail.com>
2025-06-18 19:23:36 +0530

    ..

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import './App.css';
 import Front_p from './assets/page1/front_p.jsx';
 import CustomCursor from './assets/curs/CustomCursor.jsx';
-
+import Navbar from './assets/page1/navbar/navbar.jsx';
 function App() {
   return (
     <div>
       <CustomCursor />
-      <div>
-        <h1>hello</h1>
-      </div>
+      <Navbar />
       <Front_p />
     </div>
   );
